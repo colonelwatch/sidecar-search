1c8537e091cf4d41fa2d538150a412ac3c101a5a
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2026-04-04 20:25:06 -0700

    Test (and handle edge cases in) datasets_utils
    Testing of the `iter_tensors` function does not check whether iteration
    is lazy. This would require mocking, but there is more than one path
    to triggering lazy iteration, making mocking difficult. `batch_size`
    option added to `iter_tensors` to facilitate testing.
    
    `resolve_dimensions` explicitly surfaces a couple exceptions that would
    have been encountered anyway, converting them to `TypeError`. Also now
    raises when a dataset with a variable-length embedding is passed in
    (represented by `dimensions == -1`).
    
    The test for `load_dataset` passes if line to sort `paths` is added, but
    it fails otherwise. This pass case was validated by modifying the code,
    and a general change to deal with glob outputs having arbitrary order is
    upcoming. After that, the test should pass.

--- sidecar_search/index/utils/datasets_utils.py
+++ sidecar_search/index/utils/datasets_utils.py
@@ -1,38 +1,57 @@
 from pathlib import Path
-from typing import TYPE_CHECKING, Generator, cast
+from typing import Generator, cast
 
 import numpy as np
 import torch
-from datasets import Dataset
-
-if TYPE_CHECKING:
-    from datasets.utils.typing import PathLike as HfPathLike  # NOTE: undocumented!
+from datasets import Dataset, List
+from datasets.utils.typing import PathLike as HfPathLike  # NOTE: undocumented!
 
 BATCH_SIZE = 1024
 
 
 def load_dataset(dir: Path) -> Dataset:
-    paths: list[HfPathLike] = [str(path) for path in dir.glob("*.parquet")]
+    paths = [str(path) for path in dir.glob("*.parquet")]
+    if not paths:  # schema inference is impossible without at least one file
+        raise ValueError("must pass folder with at least one parquet file")
+
+    # NOTE: `.from_parquet` has list[HfPathLike] in its signature, so the intention is
+    #       clearly covariance (mixed list makes little sense), but `list` is invariant
+    paths = cast(list[HfPathLike], paths)
     dataset = Dataset.from_parquet(paths)
     assert isinstance(dataset, Dataset), (
         "datasets violated documentation about return type"
     )
+
     ids = np.arange(len(dataset), dtype=np.int32)  # add unique integer IDs for later
     return dataset.add_column("index", ids)  # type: ignore # datasets func sig is wrong
 
 
 # TODO: resolve this sooner than later
 def resolve_dimensions(dataset: Dataset, dimensions: int | None) -> int:
-    if dimensions is None:
-        dimensions = len(dataset[0]["embedding"])
+    if dimensions is not None:
+        return dimensions
+
+    try:
+        feature = dataset.features["embedding"]
+    except KeyError as e:
+        raise TypeError('dataset features does not contain "embedding" field') from e
+    if not isinstance(feature, List):
+        raise TypeError('"embedding" field is not list type')
+
+    dimensions = feature.length
+    if dimensions < 0:
+        raise TypeError('"embedding" field does not have fixed length')
+
     return dimensions
 
 
 def iter_tensors(
-    dataset: Dataset,
+    dataset: Dataset, batch_size: int | None = None
 ) -> Generator[tuple[torch.Tensor, torch.Tensor], None, None]:
+    if batch_size is None:
+        batch_size = BATCH_SIZE
     cols = dataset.select_columns(["index", "embedding"])
     cols.set_format("torch")
-    for batch in cols.iter(BATCH_SIZE):
+    for batch in cols.iter(batch_size):
         batch = cast(dict[str, torch.Tensor], batch)
         yield batch["index"], batch["embedding"]
